6074268f1bb0b921ef598b38c2a09e76c9c0d10f
barrykeegan <barry.keegan@gmail.com>
2020-01-22 12:25:14 +0000

    Removed some console.log outputs

--- cryptoutils.js
+++ cryptoutils.js
@@ -52,16 +52,7 @@ function convertString(str, shiftVal)
 
 function performFreqAnalysis(msg)
 {
-    console.log(msg);
     var letterFreq = [];
-    /*var letterFreq = [['a', 0, 0], ['b', 0, 0], ['c', 0, 0], ['d', 0, 0], ['e', 0, 0], ['f', 0, 0], ['g', 0, 0], ['h', 0, 0], 
-                    ['i', 0, 0], ['j', 0, 0], ['k', 0, 0], ['l', 0, 0], ['m', 0, 0], ['n', 0, 0], ['o', 0, 0], ['p', 0, 0], 
-                    ['q', 0, 0], ['r', 0, 0], ['s', 0, 0], ['t', 0, 0], ['u', 0, 0], ['v', 0, 0], ['w', 0, 0], ['x', 0, 0], 
-                    ['y', 0, 0], ['z', 0, 0], ];*/
-    /*console.log([['a', 0, 0], ['b', 0, 0], ['c', 0, 0], ['d', 0, 0], ['e', 0, 0], ['f', 0, 0], ['g', 0, 0], ['h', 0, 0], 
-                    ['i', 0, 0], ['j', 0, 0], ['k', 0, 0], ['l', 0, 0], ['m', 0, 0], ['n', 0, 0], ['o', 0, 0], ['p', 0, 0], 
-                    ['q', 0, 0], ['r', 0, 0], ['s', 0, 0], ['t', 0, 0], ['u', 0, 0], ['v', 0, 0], ['w', 0, 0], ['x', 0, 0], 
-                    ['y', 0, 0], ['z', 0, 0], ]);*/
     var totalLetters = 0;
     
     for (var i = 0; i < 26; i++)
@@ -86,7 +77,6 @@ function performFreqAnalysis(msg)
             letterFreq[i][2] = ((letterFreq[i][1] / totalLetters) * 100).toFixed(3);
         }
     }
-    console.log(letterFreq);
     return letterFreq;
 }
 
