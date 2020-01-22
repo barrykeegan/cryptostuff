c3a3e5ce751c9aa820ae3041caf97c769cc0ca82
barrykeegan <barry.keegan@gmail.com>
2020-01-22 07:38:08 +0000

    Removed some white space cryptutils

--- cryptoutils.js
+++ cryptoutils.js
@@ -4,50 +4,50 @@ var engCharFreq = [['E', 12.02], ['T', 9.10], ['A', 8.12], ['O', 7.68], ['I', 7.
                     ['K', 0.69], ['X', 0.17], ['Q', 0.11], ['J', 0.10], ['Z', 0.07],];
 
 function shiftChar(char, shiftVal)
+{
+    var currCharPos = char.charCodeAt(0) - "a".charCodeAt(0);
+    if(shiftVal !== 0)
+    {
+        if (shiftVal > 0)
+        {
+            var newCharPos=(currCharPos + shiftVal) % 26;
+            return String.fromCharCode("a".charCodeAt(0) + newCharPos);
+        }
+        else
+        {
+            var newCharPos = currCharPos + shiftVal;
+            if (newCharPos < 0)
             {
-                var currCharPos = char.charCodeAt(0) - "a".charCodeAt(0);
-                if(shiftVal !== 0)
-                {
-                    if (shiftVal > 0)
-                    {
-                        var newCharPos=(currCharPos + shiftVal) % 26;
-                        return String.fromCharCode("a".charCodeAt(0) + newCharPos);
-                    }
-                    else
-                    {
-                        var newCharPos = currCharPos + shiftVal;
-                        if (newCharPos < 0)
-                        {
-                            return String.fromCharCode("a".charCodeAt(0) + newCharPos + 26);
-                        }
-                        else
-                        {
-                            return String.fromCharCode("a".charCodeAt(0) + newCharPos);
-                        }
-                    }
-                }
-                else
-                {
-                    return char;
-                }
+                return String.fromCharCode("a".charCodeAt(0) + newCharPos + 26);
             }
+            else
+            {
+                return String.fromCharCode("a".charCodeAt(0) + newCharPos);
+            }
+        }
+    }
+    else
+    {
+        return char;
+    }
+}
 
 
-            function convertString(str, shiftVal)
-            {
-                var converted = "";
-                for (var i = 0; i < str.length; i++)
-                {
-                    if(str.charAt(i) >= 'a' && str.charAt(i) <= 'z')
-                    {
-                        converted += shiftChar(str.charAt(i),shiftVal)
-                    }
-                    else
-                    {
-                        converted += str.charAt(i);
-                    }
-                }
-                return converted;
-            }
+function convertString(str, shiftVal)
+{
+    var converted = "";
+    for (var i = 0; i < str.length; i++)
+    {
+        if(str.charAt(i) >= 'a' && str.charAt(i) <= 'z')
+        {
+            converted += shiftChar(str.charAt(i),shiftVal)
+        }
+        else
+        {
+            converted += str.charAt(i);
+        }
+    }
+    return converted;
+}
 
             
