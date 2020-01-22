d0acc7b868895b0e58ac614092832d8a881d4015
barrykeegan <barry.keegan@gmail.com>
2020-01-22 11:01:37 +0000

    updated frequency analysis page to allow for changing best guess

--- cryptoutils.js
+++ cryptoutils.js
@@ -50,4 +50,44 @@ function convertString(str, shiftVal)
     return converted;
 }
 
+function performFreqAnalysis(msg)
+{
+    console.log(msg);
+    var letterFreq = [];
+    /*var letterFreq = [['a', 0, 0], ['b', 0, 0], ['c', 0, 0], ['d', 0, 0], ['e', 0, 0], ['f', 0, 0], ['g', 0, 0], ['h', 0, 0], 
+                    ['i', 0, 0], ['j', 0, 0], ['k', 0, 0], ['l', 0, 0], ['m', 0, 0], ['n', 0, 0], ['o', 0, 0], ['p', 0, 0], 
+                    ['q', 0, 0], ['r', 0, 0], ['s', 0, 0], ['t', 0, 0], ['u', 0, 0], ['v', 0, 0], ['w', 0, 0], ['x', 0, 0], 
+                    ['y', 0, 0], ['z', 0, 0], ];*/
+    /*console.log([['a', 0, 0], ['b', 0, 0], ['c', 0, 0], ['d', 0, 0], ['e', 0, 0], ['f', 0, 0], ['g', 0, 0], ['h', 0, 0], 
+                    ['i', 0, 0], ['j', 0, 0], ['k', 0, 0], ['l', 0, 0], ['m', 0, 0], ['n', 0, 0], ['o', 0, 0], ['p', 0, 0], 
+                    ['q', 0, 0], ['r', 0, 0], ['s', 0, 0], ['t', 0, 0], ['u', 0, 0], ['v', 0, 0], ['w', 0, 0], ['x', 0, 0], 
+                    ['y', 0, 0], ['z', 0, 0], ]);*/
+    var totalLetters = 0;
+    
+    for (var i = 0; i < 26; i++)
+    {
+        letterFreq.push([String.fromCharCode("a".charCodeAt(0) + i), 0, 0]);
+    }
+    
+    
+    for (var c= 0; c < msg.length; c++)
+    {
+        if(msg[c] >= 'a' && msg[c] <= 'z')
+        {
+            totalLetters++;
+            letterFreq[msg[c].charCodeAt(0)-"a".charCodeAt(0)][1]++;
+        }
+    }
+
+    for (var i = 0; i < letterFreq.length; i++)
+    {
+        if(letterFreq[i][1] !== 0)
+        {
+            letterFreq[i][2] = ((letterFreq[i][1] / totalLetters) * 100).toFixed(3);
+        }
+    }
+    console.log(letterFreq);
+    return letterFreq;
+}
+
             
